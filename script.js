1b161f0508f728ed5e699abc5b9b0fe1bcacead1
root <root@localhost.localdomain>
2019-03-27 13:56:54 +0300

    END

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$("#menu").click(function() {
+/*$("#menu").click(function() {
     $(".icon").animate({opacity: "0"}, 1000);
     setTimeout(function(){
       if ( $("#myTopnav").hasClass('topnav responsive') ){
@@ -9,4 +9,23 @@ $("#menu").click(function() {
    }   }, 1000);
 
     $(".icon").animate({opacity: "1"}, 1000);
-});
+});*/
+
+
+window.onscroll = function(){
+  myFunc();
+};
+
+
+  var header = document.getElementById('header');
+  var sticky = header.offsetTop;
+
+function myFunc(){
+  if(window.pageYOffset > sticky){
+    header.classList.add('sticky');
+    header.classList.add('bord');
+  }else{
+    header.classList.remove('sticky');
+    header.classList.remove('bord');
+  }
+}
